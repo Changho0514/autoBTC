ae23c71c7146da9a369f8afa25ccb656054d3b1b
Changho0514 <chh9450@naver.com>
2024-10-15 14:56:50 +0900

    feat - 유튜브 자막 추출 방법 리스트로 변경

--- youtube_data.py
+++ youtube_data.py
@@ -1,13 +1,18 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
-def get_combined_transcript(video_id):
+def get_combined_transcript(playlist):
+    subscribes = []
     try:
-        playlist = ['6itriowPhhM', 'Ln2PevCHEuU', 'Li3EV0YVuSg']
-    
-        sentences = YouTubeTranscriptApi.get_transcript("6itriowPhhM", languages=['ko'])
-
-        # 모든 텍스트를 하나의 문자열로 결합
-        combined_text = " ".join([sentence['text'] for sentence in sentences])
+        for video_id in playlist:
+            sentences = YouTubeTranscriptApi.get_transcript(video_id, languages=['ko'])
+            # 각 비디오의 텍스트를 하나의 문자열로 결합하여 리스트에 추가
+            combined_text = " ".join([sentence['text'] for sentence in sentences])
+            subscribes.append(combined_text)
     except Exception as e:
-        logger.error(f"Error fetching youTube transcript: {e}")
-        return ""
+        print(f"Error fetching YouTube transcript for video {video_id}: {e}")
+    
+    return subscribes
+
+# 사용 예시
+playlist = ['6itriowPhhM', 'Ln2PevCHEuU', 'Li3EV0YVuSg', '3XbtEX3jUv4']
+transcripts = get_combined_transcript(playlist)
